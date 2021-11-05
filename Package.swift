8f9b68d6d4613f9aa84d7d0dc59a423ea362ca74
Kireyin <kireyin@icloud.com>
2021-11-05 16:16:08 +0100

    no message

--- Package.swift
+++ Package.swift
@@ -2,20 +2,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "UniversalSDK",
+    name: "LSUniversalSDK",
     platforms: [
         .iOS(.v13),
     ],
     products: [
         .library(
-            name: "UniversalSDK",
-            targets: ["UniversalSDK"]
+            name: "LSUniversalSDK",
+            targets: ["LSUniversalSDK"]
         )
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "UniversalSDK",
+            name: "LSUniversalSDK",
             url: "https://sightcall-ios-cocoapods.s3.amazonaws.com/universalSDK/2.6.3/sdk.zip",
             checksum: "dd70aa3ad3b1945163f4db8e674ed0dc98c50a6af24896edce7f3d7f88b2c535"
         )
